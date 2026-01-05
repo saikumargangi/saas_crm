dd5955aa38493b1bfc18cb3f105f8548026f1860
Sai Kumar Gangipamula <saikumargangipamula@Sais-MacBook-Air.local>
2026-01-06 01:49:21 +0400

    Config: Update for Docker/Codespaces compatibility. Add Proxy support.

--- web/next.config.js
+++ web/next.config.js
@@ -49,7 +49,7 @@ const nextConfig = {
         return [
             {
                 source: '/api/v1/:path*',
-                destination: 'http://localhost:8000/api/v1/:path*',
+                destination: process.env.INTERNAL_API_URL || 'http://localhost:8000/api/v1/:path*',
             },
         ];
     },
